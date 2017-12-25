dda05ae95f7d4183ac2b71881bc0624b76aff7de
Windy <19060@qq.com>
2017-12-25 22:37:05 +0800

    remove un support download db

--- cnki-downloader/src/content.js
+++ cnki-downloader/src/content.js
@@ -5,10 +5,9 @@
 // });
 
 
-
 // load button
-var insertBtn = function insertBtn() {
-    if(typeof $ !== 'undefined' && window && window.frames && window.frames['iframeResult']){
+var insertBtn = function insertBtn () {
+    if (typeof $ !== 'undefined' && window && window.frames && window.frames['iframeResult']) {
 
         var lines = $(window.frames['iframeResult'].document).find('.GridTableContent tr');
         lines.each(function () {
@@ -22,30 +21,34 @@ var insertBtn = function insertBtn() {
             var dbCode = url.match(/DbCode=(.+?)&/)[1];
             dbCode = {
                 CLFQ: 'literatures', //文献
-                CJFQ: 'journals', //期刊
+                //CJFQ: 'journals', //期刊
                 CDFD: 'doctortheses', //博士
                 CMFD: 'mastertheses', //硕士
                 CCFD: 'conferences', //会议
-                CCND: 'newspapers' // 报纸
+                //CCND: 'newspapers', // 报纸
+                //SCPD: '',//中国专利
+                //CPFD: '',//中国会议
             }[dbCode];
             //a.before('<a class="briefDl_Y" target="_blank" onclick="window.cnki.GetArticle(\'' + dbCode + '\', \'' + fileName + '\')"></a>');
 
+            if(dbCode){
+                var link_dom = document.createElement('a');
+                link_dom.setAttribute('class', 'briefDl_Y');
+                link_dom.addEventListener('click', function () {
 
-            var link_dom = document.createElement('a');
-            link_dom.setAttribute('class','briefDl_Y');
-            link_dom.addEventListener('click',function () {
-
-                chrome.runtime.sendMessage(
-                    { action: 'download', dbCode: dbCode, fileName: fileName },
-                    function (response) {
-                        console.log('response', response);
-                    }
-                );
+                    chrome.runtime.sendMessage(
+                        { action: 'download', dbCode: dbCode, fileName: fileName },
+                        function (response) {
+                            console.log('response', response);
+                        }
+                    );
 
 
-            });
-            var a_node = a.get(0);
-            a_node.parentElement.insertBefore(link_dom, a_node);
+                });
+                var a_node = a.get(0);
+                a_node.parentElement.insertBefore(link_dom, a_node);
+            }
+
 
             a.attr('inserted', '1');
             console.log('btn inserted! ' + a.text());
